6a8862d8ac3a682c266aa2c6b392fd90a252615a
Thomas <thomas.rundle@conduitlearning.com>
2016-09-20 14:04:27 -0500

    Trying to get the project-list to highlight when navigated to via browser URL

--- app/projects/project-list.component.js
+++ app/projects/project-list.component.js
@@ -9,27 +9,51 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
 var project_service_1 = require('./project.service');
 var ProjectListComponent = (function () {
-    function ProjectListComponent(projectService) {
+    function ProjectListComponent(route, router, projectService) {
+        this.route = route;
+        this.router = router;
         this.projectService = projectService;
     }
     ProjectListComponent.prototype.ngOnInit = function () {
-        this.getProjects();
+        var _this = this;
+        this.route.params.forEach(function (params) {
+            _this.selectedId = +params['id'];
+            _this.getProjects();
+        });
     };
     ProjectListComponent.prototype.getProjects = function () {
         var _this = this;
         this.projectService.getProjects()
             .then(function (projects) {
             _this.projects = projects;
+            if (_this.selectedId) {
+                var id = _this.selectedId;
+                _this.selectProject(id);
+            }
+        });
+    };
+    ProjectListComponent.prototype.onSelect = function (project) {
+        this.router.navigate(['/projects', project.id]);
+        this.selectedId = project.id;
+    };
+    ProjectListComponent.prototype.isSelected = function (project) {
+        return project.id === this.selectedId;
+    };
+    ProjectListComponent.prototype.selectProject = function (id) {
+        this.selectedProject = this.projects.find(function (project) {
+            return project.id === id;
         });
     };
     ProjectListComponent = __decorate([
         core_1.Component({
             selector: 'project-list',
-            template: "\n        <h2>Projects Overview</h2>\n        <p>Summary of projects here</p>\n        <router-outlet></router-outlet>\n        <h3>LIST OF PROJECTS</h3>\n        <p>Table listing all the projects</p>\n        <table class=\"table\">\n            <thead>\n                <tr>\n                    <th>Name</th>\n                    <th>Date</th>\n                    <th>Astronauts</th>\n                    <th>...</th>\n                </tr>\n            </thead>\n            <tbody>\n                <tr *ngFor=\"let project of projects\">\n                    <td>{{project.name}}</td>\n                    <td>{{project.mission_date}}</td>\n                    <td>{{project.personnel[0]}}</td>\n                    <td>\n                        <button class=\"btn btn-default\">view details</button>\n                    </td>\n                </tr>\n            </tbody>\n        </table>\n    "
+            template: "\n        <h2>Projects Overview</h2>\n        <p>Summary of projects here</p>\n        <router-outlet></router-outlet>\n        <h3>LIST OF PROJECTS</h3>\n        <p>Table listing all the projects</p>\n        <table class=\"table\">\n            <thead>\n                <tr>\n                    <th>Name</th>\n                    <th>Date</th>\n                    <th>Astronauts</th>\n                    <th>...</th>\n                </tr>\n            </thead>\n            <tbody>\n                <tr *ngFor=\"let project of projects\"\n                    [class.selected]=\"isSelected(project)\"\n                    (click)=\"onSelect(project)\">\n                    <td>{{project.name}}</td>\n                    <td>{{project.mission_date}}</td>\n                    <td>{{project.personnel[0]}}</td>\n                    <td>\n                        <button class=\"btn btn-default\">view details</button>\n                    </td>\n                </tr>\n            </tbody>\n        </table>\n    ",
+            styles: ["\n        .selected {\n            background-color: #CFD8DC;\n            color: white;\n        }\n    "]
         }), 
-        __metadata('design:paramtypes', [project_service_1.ProjectService])
+        __metadata('design:paramtypes', [router_1.ActivatedRoute, router_1.Router, project_service_1.ProjectService])
     ], ProjectListComponent);
     return ProjectListComponent;
 }());
--- app/projects/project-list.component.ts
+++ app/projects/project-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 import { Project } from './project';
 import { ProjectService } from './project.service';
@@ -21,7 +22,9 @@ import { ProjectService } from './project.service';
                 </tr>
             </thead>
             <tbody>
-                <tr *ngFor="let project of projects">
+                <tr *ngFor="let project of projects"
+                    [class.selected]="isSelected(project)"
+                    (click)="onSelect(project)">
                     <td>{{project.name}}</td>
                     <td>{{project.mission_date}}</td>
                     <td>{{project.personnel[0]}}</td>
@@ -31,20 +34,50 @@ import { ProjectService } from './project.service';
                 </tr>
             </tbody>
         </table>
-    `
+    `,
+    styles: [`
+        .selected {
+            background-color: #CFD8DC;
+            color: white;
+        }
+    `]
 })
 export class ProjectListComponent implements OnInit { 
+    private selectedId: number;
     constructor(
+        private route: ActivatedRoute,
+        private router: Router,
         private projectService: ProjectService
     ) { }
     projects: Project[];
+    selectedProject: Project;
     ngOnInit(): void {
-        this.getProjects();
+        this.route.params.forEach( (params: Params) => {
+            this.selectedId = +params['id'];
+            this.getProjects();
+        })
+        
     }
     getProjects(): void {
         this.projectService.getProjects()
             .then ( (projects) => {
                 this.projects = projects;
+                if (this.selectedId) {
+                    let id = this.selectedId;
+                    this.selectProject(id);
+                }
             });
+    }
+    onSelect(project: Project): void {
+        this.router.navigate(['/projects', project.id]);
+        this.selectedId = project.id;
+    }
+    isSelected(project: Project) {
+        return project.id === this.selectedId;
+    }
+    selectProject(id: number) {
+        this.selectedProject = this.projects.find( (project) => {
+            return project.id === id;
+        });
     }
 }

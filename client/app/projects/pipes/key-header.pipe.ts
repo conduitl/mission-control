af68e3b13400d2377284960015a3c9aecda8baf0
Thomas <thomas.rundle@conduitlearning.com>
2016-11-12 14:25:53 -0600

    Remove key header pipe which has been replaced

--- client/app/projects/pipes/key-header.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'keyHeader'})
-export class KeyHeaderPipe implements PipeTransform {
-    transform(value: string) {
-        return value.toUpperCase().replace(/_/g, ' ');
-    }
-}

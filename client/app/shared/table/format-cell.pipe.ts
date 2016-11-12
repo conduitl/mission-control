6d0552e64d8ef0976008d1044ebeba1c8f218ff2
Thomas <thomas.rundle@conduitlearning.com>
2016-11-12 14:57:42 -0600

    Change missed in prev commit

--- client/app/shared/table/format-cell.pipe.ts
+++ client/app/shared/table/format-cell.pipe.ts
@@ -21,15 +21,30 @@ export class FormatCellPipe implements PipeTransform {
                 }
             }
             // TODO: Allow config for handling nested objects
+            // TODO: ID instance of Date
             if ( typeof value === "object") {
-                return value.name
+                return value.name ? value.name : 'Unknown object';
             }
+            // If default & not other cond, return value with no transform
+            return value;
+        }
+
+        /* Dates */
+        if (format.slice(0, 4) === 'date') {
+            let arg = format.split(':')[1];
+            if (arg) {
+                return this.datePipe.transform(value, arg);
+            }
+            return this.datePipe.transform(value, 'fullDate');
         }
         
-        if (format === 'currency') {
-            return this.currencyPipe.transform(value, 'USD', true);
+        /* Currency */
+        if (format.slice(0, 8) === 'currency') {
+            let args = format.split(':').slice(1);
+            return this.currencyPipe.transform(value, args[0], isTrue(args[1]), args[2] );
         }
-        
+
+        // If no cond met, simply return value
         return value;
 
         // helper function
